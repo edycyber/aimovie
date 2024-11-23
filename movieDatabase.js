1bf7db5edcef124faed104afbbd622a34e90c9cc
edycyber <foodworldmeta@gmail.com>
2024-11-23 14:28:19 +0100

    Create movieDatabase.js

---
+++ movieDatabase.js
@@ -0,0 +1,22 @@
+const aiMovieDatabase = {
+    "fiction": {
+        title: "Alien",
+        description: "Comprehensive documentary about artificial intelligence revolution",
+        url: "https://www.youtube.com/embed/k5ahSzsMluI"
+    },
+    "real movies": {
+        title: "Harry Potter and Volcano Witch",
+        description: "Documentary about the current state of AI",
+        url: "https://www.youtube.com/embed/fGKAJrbiMJI"
+    },
+    "scary": {
+        title: "Harry Potter and The Founding of Hogwarts",
+        description: "Exploring the future possibilities of artificial intelligence",
+        url: "https://www.youtube.com/embed/xr1-ToY24Cg"
+    },
+    "other": {
+        title: "Machine Learning Revolution",
+        description: "Deep dive into machine learning technologies",
+        url: "https://www.youtube.com/embed/5dZ_lvDgevk"
+    }
+};
